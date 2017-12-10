760115344ea97686d5ed64a0843f55553743ed5d
whf191 <1296935454@qq.com>
2017-12-10 10:42:25 +0800

    配置ckeditor的config.js图片上传参数

--- gengyun/static/ckeditor/config.js
+++ gengyun/static/ckeditor/config.js
@@ -7,4 +7,10 @@ CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
+    config.extraPlugins = 'uploadimage';
+	config.filebrowserImageUploadUrl = '/liulan/';
+    config.filebrowserBrowseUrl =  '/liulan/';
+	config.uploadUrl = '/uploader/';
+
+
 };
